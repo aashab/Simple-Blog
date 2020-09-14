959ed78ce3b8add289884e0d3298e33e191fd38c
Aashab Tajwar <hqhunter780@gmail.com>
2020-09-14 23:44:35 +0600

    new files

--- routes/questions.js
+++ routes/questions.js
@@ -8,6 +8,13 @@ router.get('/new', function(req, res) {
     res.render('questions/new.ejs')
 });
 
+// ROUTE FOR EDITING A POST
+router.get('/edit/:id', async function(req, res) {
+    // find the question by id
+    const question = await Question.findById(req.params.id);
+    res.render('questions/edit.ejs', { question: question });
+})
+
 router.post('/', function(req, res) {
     console.log(req.body.title);
     console.log(req.body.description);
@@ -26,6 +33,32 @@ router.post('/', function(req, res) {
             console.log(err)
         })
 });
+
+// WAS WORKING HERE!
+
+
+router.put('/:id', async function(req, res) {
+    // finding by id
+    let question = await Question.findById(req.params.id);
+    question.title = req.body.title;
+    question.description = req.body.description;
+    question.text = req.body.text;
+    question.save()
+        .then(function(result) {
+            res.redirect(`/questions/${question.id}`);
+        }).catch(function(err) {
+            console.log(err)
+        })
+    /*
+    try {
+        question = await question.save();
+        res.redirect(`/questions/${question.id}`);
+
+    } catch(e) {
+        console.log(e);
+    }*/
+});
+
 
 //  TO SHOW A QUESTION
 router.get('/:id', async function(req, res) {
